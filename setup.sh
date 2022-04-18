131df398838a026703c190dbbc86110adf8cce0d
Jules Chéron <43635101+jules-ch@users.noreply.github.com>
2022-04-18 18:29:13 +0200

    Update setup.sh
    Update NodeJS LTS

--- setup.sh
+++ setup.sh
@@ -159,7 +159,7 @@ sudo flatpak install postman -y
 ## Node.JS + Yarn Install
 
 echo "Installing Node 14 JS LTS"
-curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo apt-get install -y nodejs 
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list 
