92e19ec0fa747adadacdbedb4be64d91e0903049
Jules Chéron <jules.cheron@gmail.com>
2023-09-11 23:15:34 +0200

    chore: remove material shell for a cleaner setup
    - Add new script for nodejs install

--- setup.sh
+++ setup.sh
@@ -158,18 +158,21 @@ sudo flatpak install postman -y
 
 ## Node.JS + Yarn Install
 
-echo "Installing Node 14 JS LTS"
-curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-sudo apt-get install -y nodejs 
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl gnupg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+
+NODE_MAJOR=18
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+
+echo "Installing Node 18 JS LTS"
+sudo apt-get update
+sudo apt-get install nodejs -y
+
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list 
 sudo apt-get update -qq && sudo apt-get install -y yarn
-
-
-# Setup GNOME material shell (Need Node.js for compilation of the Typescript extension)
-
-git clone -b 3.38 https://github.com/PapyElGringo/material-shell.git ~/material-shell || true
-make -C ~/material-shell/ install
 
 
 # Setup Android Studio for Mobile Development
